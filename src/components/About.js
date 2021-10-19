c8f40a4b922e2f4fd18b37916be8a90b77478473
Steve Fokas <sf487552@gmail.com>
2021-10-19 11:52:59 -0400

    SDFF

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function About() {
 	return (
-        <div class="display centered" id="about">
-            <p class="grow txtshadow" txtshadowid="text-info">"I am a Full Stack Web Developer who is focused on creating unique and intuitive experiences
+        <div id="about">
+            <p>"I am a Full Stack Web Developer who is focused on creating unique and intuitive experiences
             for all types of users. I am currently earning my certification in full stack development at the University of Pennsylvania where I have cultivated my skills in Javascript, CSS, and HTML. Years of creative problem solving have helped me to develop a unique perspective which allows me to develop original, eye-catching and intuitively designed web tools that keeps users engaged. Creative design projects come naturally to me thanks to the skills I’ve developed as a successful artist, music producer, and sound engineer with a diverse portfolio of projects that have seen success across the globe in over 85 different countries. My communication skills have also blossomed over the years as I've created a body work that translates easily and can be enjoyed by a diverse audience from all walks of life. I look forward to using my unique skills to develop innovative web pages and platforms for my users!"</p>
         </div>
     );
