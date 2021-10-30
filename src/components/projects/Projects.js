fd43f182739ec1cff43fe77d1a120cd72494d3d0
Steve Fokas <sf487552@gmail.com>
2021-10-30 08:24:33 -0400

    styling fix

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../projects/projects.css';
-import image1 from '../images/mysite1.jpg'
-import image2 from '../images/website image.jpg'
+import image1 from '../images/musicsite2.jpg';
+import image2 from '../images/fitnessapp.jpg';
+import image3 from '../images/cocktailquiz.jpg';
+import image4 from '../images/weatherdash.jpg';
+import image5 from '../images/notetaker.jpg';
 
 
 
@@ -10,9 +13,9 @@ function Projects() {
         <div className="container">
             <img className="grow" src={image1} alt="site1"></img>
             <img className="grow" src={image2} alt="site2"></img>
-            <img className="grow" src={image1} alt="site3"></img>
-            <img className="grow" src={image1} alt="site4"></img>
-            <img className="grow" src={image1} alt="site5"></img>
+            <img className="grow" src={image3} alt="site3"></img>
+            <img className="grow" src={image4} alt="site4"></img>
+            <img className="grow" src={image5} alt="site5"></img>
         </div>
     );
 }
--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -3,17 +3,11 @@ import '../resume/resume.css';
 
 function Resume() {
 	return (
-        <div className="card-column centered" id="resume">
-            <figure className="card code-card">
-                <h2 className="card-header">Resume</h2>
-                <div className="card-body">
-                    <i className="menu"></i>
-                    <a className="view-resume"
-                        href="https://docs.google.com/document/d/1gU-R_xrsGzjy3LKaFQup1oYX_ipGa3HljVCatWf2Bpc/edit?usp=sharing"
-                        target="_blank" rel="noreferrer">Click To View
-                    </a>
-                </div>
-            </figure>
+        <div className="myResume" id="resume">
+            <a className="view-resume"
+                href="https://docs.google.com/document/d/1gU-R_xrsGzjy3LKaFQup1oYX_ipGa3HljVCatWf2Bpc/edit?usp=sharing"
+                target="_blank" rel="noreferrer">Click To View
+            </a>
         </div>
     );
 }
