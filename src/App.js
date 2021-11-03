dfc6cea861803ed63744e6cd1605502c298199b3
Steve Fokas <sf487552@gmail.com>
2021-11-03 16:44:42 -0400

    servers

--- src/App.js
+++ src/App.js
@@ -14,9 +14,33 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./components/home/Home";
+import TagManager from 'react-gtm-module'
+const tagManagerArgs = {
+  gtmId: '<GTM-5J74PFD>'
+}
+TagManager.initialize(tagManagerArgs)
 
 
 class App extends Component {
+  state = {
+      data: null
+    };
+  
+    componentDidMount() {
+      this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
+      // fetching the GET route from the Express server which matches the GET route from server.js
+    callBackendAPI = async () => {
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      return body;
+    };
   state = {
     sidebarOpen: false
   };
