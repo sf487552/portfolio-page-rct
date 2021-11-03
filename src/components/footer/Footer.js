14f0ef41d06f3bda78345c7d68e98d1e4e686262
Steve Fokas <sf487552@gmail.com>
2021-11-03 18:56:00 -0400

    link fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
                 <ul class="social">
                     <li><a id="social-icons shadow" className="slink" href="https://www.linkedin.com/in/steven-fokas-01933849/"><i className="grow fa fa-linkedin" aria-hidden="true"></i> </a></li>               
                     <li><a id="social-icons shadow" className="slink" href="https://github.com/sf487552"><i className="grow fa fa-github" aria-hidden="true"></i> </a></li>
-                    <li><a id="social-icons shadow" className="slink" href="steven.fokas89@gmail.com"><i className="grow fa fa-envelope" aria-hidden="true"></i> </a></li>
+                    <li><a id="social-icons shadow" className="slink" href="mailto:steven.fokas89@gmail.com"><i className="grow fa fa-envelope" aria-hidden="true"></i> </a></li>
                     <li><a id="social-icons shadow" className="slink" href="tel:610-653-0662"><i className="grow fa fa-phone" aria-hidden="true"></i> </a></li> 
                 </ul>
                 <script src="script.js"></script>
