815d8e72cdd7cebe3559f76d53bba27a064bba67
Steve Fokas <sf487552@gmail.com>
2021-11-04 08:35:38 -0400

    adsfgg

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+// server.js
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
 
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
